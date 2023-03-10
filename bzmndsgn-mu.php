9335ebfcd6a380e838d6f6a213d8e7fc105b3d5a
Saul Baizman <saul@baizmandesign.com>
2023-03-09 22:03:07 -0500

    Refined list of constants.
    Removed constants which must be in wp-config.php. Added two new constants.

--- bzmndsgn-mu.php
+++ bzmndsgn-mu.php
@@ -6,17 +6,16 @@
  * @version 1.0.0
  */
 
-if ( ! defined ( 'WP_DEBUG' ) ) { 
-	define( 'WP_DEBUG', true );
-}
-
-if ( ! defined ( 'WP_DEBUG_LOG' ) ) { 
- define( 'WP_DEBUG_LOG', 'debug.log' );
-}
-
-if ( ! defined ( 'WP_DEBUG_DISPLAY' ) ) { 
-	define( 'WP_DEBUG_DISPLAY', true );
-}
+/*
+Constants that don't work in this file / must be located in wp-config.php:
+	+ WP_DEBUG
+	+ WP_DEBUG_LOG
+	+ WP_DEBUG_DISPLAY
+	+ SCRIPT_DEBUG
+	+ WP_MEMORY_LIMIT
+	+ WP_MAX_MEMORY_LIMIT
+	+ WP_CACHE
+*/
 
 if ( ! defined ( 'JETPACK_STAGING_MODE' ) ) { 
 	define( 'JETPACK_STAGING_MODE', true );
@@ -31,15 +30,7 @@ if ( ! defined ( 'WP_POST_REVISIONS' ) ) {
 	define( 'WP_POST_REVISIONS', false );
 }
 
-if ( ! defined ( 'SCRIPT_DEBUG' ) ) { 
-	define( 'SCRIPT_DEBUG', true );
-}
-
 // # https://kinsta.com/blog/wp-query/
-if ( ! defined ( 'SAVEQUERIES' ) ) { 
-	define( 'SAVEQUERIES', true );
-}
-
 if ( ! defined ( 'SAVEQUERIES' ) ) { 
 	define( 'SAVEQUERIES', true );
 }
@@ -64,21 +55,8 @@ if ( ! defined ( 'DISALLOW_FILE_EDIT' ) ) {
 	define( 'DISALLOW_FILE_EDIT', true );
 }
 
-if ( ! defined ( 'WP_MEMORY_LIMIT' ) ) { 
-	define( 'WP_MEMORY_LIMIT', '100M' ) ;
-}
-
-if ( ! defined ( 'WP_MAX_MEMORY_LIMIT' ) ) { 
-	define( 'WP_MAX_MEMORY_LIMIT', '256M' );
-}
-
 if ( ! defined ( 'EMPTY_TRASH_DAYS' ) ) { 
 	define( 'EMPTY_TRASH_DAYS', 10 );
-}
-
-// "This constant has effect only if you install a persistent caching plugin."
-if ( ! defined ( 'WP_CACHE' ) ) { 
-	define( 'WP_CACHE', true);
 }
 
 if ( ! defined ( 'IMAGE_EDIT_OVERWRITE' ) ) { 
@@ -87,4 +65,12 @@ if ( ! defined ( 'IMAGE_EDIT_OVERWRITE' ) ) {
 
 if ( ! defined ( 'AUTOMATIC_UPDATER_DISABLED' ) ) { 
 	define( 'AUTOMATIC_UPDATER_DISABLED', true );
+}
+
+if ( ! defined ( 'CONCATENATE_SCRIPTS' ) ) { 
+	define( 'CONCATENATE_SCRIPTS', false );
+}
+
+if ( ! defined ( 'ALLOW_UNFILTERED_UPLOADS' ) ) { 
+	define( 'ALLOW_UNFILTERED_UPLOADS', true ) ;
 }
