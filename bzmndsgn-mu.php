ecbdf51bbbe0bfda3a806080b8de76ec4458afb7
Saul Baizman <saul@baizmandesign.com>
2023-08-18 20:58:08 -0400

    Updated whitespace.

--- bzmndsgn-mu.php
+++ bzmndsgn-mu.php
@@ -28,61 +28,61 @@ Some constants must be located in wp-config.php and can't be placed in a must-us
 + WP_CACHE
 */
 
-if ( ! defined ( 'JETPACK_STAGING_MODE' ) ) { 
+if ( ! defined ( 'JETPACK_STAGING_MODE' ) ) {
 	define( 'JETPACK_STAGING_MODE', true );
 }
 
-if ( ! defined ( 'WP_AUTO_UPDATE_CORE' ) ) { 
+if ( ! defined ( 'WP_AUTO_UPDATE_CORE' ) ) {
 	define( 'WP_AUTO_UPDATE_CORE', false );
 }
 
 // https://kinsta.com/blog/wp-config-php/
-if ( ! defined ( 'WP_POST_REVISIONS' ) ) { 
+if ( ! defined ( 'WP_POST_REVISIONS' ) ) {
 	define( 'WP_POST_REVISIONS', false );
 }
 
 // # https://kinsta.com/blog/wp-query/
-if ( ! defined ( 'SAVEQUERIES' ) ) { 
+if ( ! defined ( 'SAVEQUERIES' ) ) {
 	define( 'SAVEQUERIES', true );
 }
 
-if ( ! defined ( 'WP_LOCAL_DEV' ) ) { 
+if ( ! defined ( 'WP_LOCAL_DEV' ) ) {
 	define( 'WP_LOCAL_DEV', true );
 }
 
-if ( ! defined ( 'DISABLE_WP_CRON' ) ) { 
+if ( ! defined ( 'DISABLE_WP_CRON' ) ) {
 	define( 'DISABLE_WP_CRON', true );
 }
 
-if ( ! defined ( 'WP_ENVIRONMENT_TYPE' ) ) { 
+if ( ! defined ( 'WP_ENVIRONMENT_TYPE' ) ) {
 	define( 'WP_ENVIRONMENT_TYPE', 'local' );
 }
 
-if ( ! defined ( 'WP_DISABLE_FATAL_ERROR_HANDLER' ) ) { 
+if ( ! defined ( 'WP_DISABLE_FATAL_ERROR_HANDLER' ) ) {
 	define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true);
 }
 
-if ( ! defined ( 'DISALLOW_FILE_EDIT' ) ) { 
+if ( ! defined ( 'DISALLOW_FILE_EDIT' ) ) {
 	define( 'DISALLOW_FILE_EDIT', true );
 }
 
-if ( ! defined ( 'EMPTY_TRASH_DAYS' ) ) { 
+if ( ! defined ( 'EMPTY_TRASH_DAYS' ) ) {
 	define( 'EMPTY_TRASH_DAYS', 10 );
 }
 
-if ( ! defined ( 'IMAGE_EDIT_OVERWRITE' ) ) { 
+if ( ! defined ( 'IMAGE_EDIT_OVERWRITE' ) ) {
 	define( 'IMAGE_EDIT_OVERWRITE', true);
 }
 
-if ( ! defined ( 'AUTOMATIC_UPDATER_DISABLED' ) ) { 
+if ( ! defined ( 'AUTOMATIC_UPDATER_DISABLED' ) ) {
 	define( 'AUTOMATIC_UPDATER_DISABLED', true );
 }
 
-if ( ! defined ( 'CONCATENATE_SCRIPTS' ) ) { 
+if ( ! defined ( 'CONCATENATE_SCRIPTS' ) ) {
 	define( 'CONCATENATE_SCRIPTS', false );
 }
 
-if ( ! defined ( 'ALLOW_UNFILTERED_UPLOADS' ) ) { 
+if ( ! defined ( 'ALLOW_UNFILTERED_UPLOADS' ) ) {
 	define( 'ALLOW_UNFILTERED_UPLOADS', true ) ;
 }
 
